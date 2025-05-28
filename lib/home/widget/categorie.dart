d3d312a5b91e98a2ba6147835d8835bf574d914f
rakotorisonlandry <rakotorisonlandry@gmail.com>
2025-05-28 08:20:29 +0200

    Update CategoriSection to fix category titles and improve ListView item rendering

--- lib/home/widget/categorie.dart
+++ lib/home/widget/categorie.dart
@@ -11,17 +11,17 @@ class CategoriSection extends StatelessWidget {
     {
       'icon': Icons.sports_motorsports_outlined,
       'color': const Color(0xFF4C8EF1),
-      'title': 'Arcade',
+      'title': 'PES',
     },
     {
       'icon': Icons.casino_outlined,
       'color': const Color(0xFF4C8EF1),
-      'title': 'Arcade',
+      'title': 'Casino',
     },
     {
       'icon': Icons.sports_esports_outlined,
       'color': const Color(0xFF4C8EF1),
-      'title': 'Arcade',
+      'title': ' Action',
     },
   ];
 
@@ -30,7 +30,7 @@ class CategoriSection extends StatelessWidget {
     return Container(
       height: 500,
       decoration: const BoxDecoration(
-        color: Colors.white,
+        // color: Colors.white,
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(30),
           topRight: Radius.circular(30),
@@ -38,17 +38,48 @@ class CategoriSection extends StatelessWidget {
       ),
       child: Column(
         children: [
+          // Liste horizontale des catégories
           Container(
             height: 50,
             color: const Color.fromARGB(255, 83, 43, 193),
             child: ListView.builder(
               itemBuilder: (context, index) {
-                return null;
+                final category = categories[index];
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                  child: Row(
+                    children: [
+                      Icon(category['icon'] as IconData, color: Colors.white),
+                      const SizedBox(width: 4),
+                      Text(
+                        category['title'] as String,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                );
               },
               itemCount: categories.length,
               scrollDirection: Axis.horizontal,
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  minimumSize: const Size(double.infinity, 50)),
+              child: const Text(
+                "Test me",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
         ],
       ),
     );
